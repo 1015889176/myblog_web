b6265bd17ec5252cc36f16d53f76a79bd9d4834c
qie <653674303@qq.com>
2020-11-18 13:46:52 +0800

    初始化

---
+++ src/views/admin/login.vue
@@ -0,0 +1,34 @@
+<!--  -->
+<template>
+    <div class="login">
+        <el-card>登录</el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {};
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {},
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {},
+    computed: {},
+    watch: {},
+    methods: {},
+    beforeCreate() {}, //生命周期 - 创建之前
+    beforeMount() {}, //生命周期 - 挂载之前
+    beforeUpdate() {}, //生命周期 - 更新之前
+    updated() {}, //生命周期 - 更新之后
+    beforeDestroy() {}, //生命周期 - 销毁之前
+    destroyed() {}, //生命周期 - 销毁完成
+    activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang='scss' scoped>
+.login {
+    font-size: 50px;
+}
+</style>
